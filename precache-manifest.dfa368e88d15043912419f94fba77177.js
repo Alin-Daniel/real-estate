df84ec66b6ebf72b80789a7316ca9c138f44ebeb
Alin-Daniel Alin-Daniel <daniel.alin100@gmail.com>
2020-03-17 17:59:20 +0200

    Updates

---
+++ precache-manifest.dfa368e88d15043912419f94fba77177.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb9bf2b5e8565cabaeb537612aee5e8a",
+    "url": "/real-estate/index.html"
+  },
+  {
+    "revision": "b0288ed7aedcac7718bd",
+    "url": "/real-estate/static/css/2.1d5701bb.chunk.css"
+  },
+  {
+    "revision": "7f8ef965c3b38796b328",
+    "url": "/real-estate/static/css/main.ce83470a.chunk.css"
+  },
+  {
+    "revision": "b0288ed7aedcac7718bd",
+    "url": "/real-estate/static/js/2.04185ee1.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/real-estate/static/js/2.04185ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8ef965c3b38796b328",
+    "url": "/real-estate/static/js/main.db6a0cc6.chunk.js"
+  },
+  {
+    "revision": "a7a0dc842d05de4bf7ee",
+    "url": "/real-estate/static/js/runtime-main.d07c5e9b.js"
+  },
+  {
+    "revision": "ab61503daff3d9772ce8fdfd2ba1a366",
+    "url": "/real-estate/static/media/customer-1.ab61503d.jpg"
+  },
+  {
+    "revision": "816b9d50adeb8ddca08d686691443694",
+    "url": "/real-estate/static/media/gal-1.816b9d50.jpg"
+  },
+  {
+    "revision": "d088735ce83100bd6c3f420bf6a7c247",
+    "url": "/real-estate/static/media/gal-2.d088735c.jpg"
+  },
+  {
+    "revision": "bdbb9fb2270731f4b7ab849714808a96",
+    "url": "/real-estate/static/media/gal-3.bdbb9fb2.jpg"
+  },
+  {
+    "revision": "3ce3a1ac54c97e1657460883a25a848a",
+    "url": "/real-estate/static/media/gal-4.3ce3a1ac.jpg"
+  },
+  {
+    "revision": "1c77f6d81d4d29f4516a463f4452ee0a",
+    "url": "/real-estate/static/media/gal-5.1c77f6d8.jpg"
+  },
+  {
+    "revision": "60018f11db64d661d4dd4949547b872f",
+    "url": "/real-estate/static/media/gal-6.60018f11.jpg"
+  },
+  {
+    "revision": "3f9c20e275e2aafe29826d7c0a21e030",
+    "url": "/real-estate/static/media/hero.3f9c20e2.jpg"
+  },
+  {
+    "revision": "7cb3ae10f84f7071a00724199953bab0",
+    "url": "/real-estate/static/media/house-1.7cb3ae10.jpg"
+  },
+  {
+    "revision": "4f78d45e7018821acd5ff03595805a70",
+    "url": "/real-estate/static/media/house-2.4f78d45e.jpg"
+  },
+  {
+    "revision": "0850c78cdccd8b0adb465e2d68619b75",
+    "url": "/real-estate/static/media/house-3.0850c78c.jpg"
+  },
+  {
+    "revision": "5defe42e600b472cdfa57bba045116e3",
+    "url": "/real-estate/static/media/house-4.5defe42e.jpg"
+  },
+  {
+    "revision": "a4b4f9886aacf2f85550498ad522e7f7",
+    "url": "/real-estate/static/media/house-5.a4b4f988.jpg"
+  },
+  {
+    "revision": "1c3f45cd0e2715d325bba57a9c8dcfd0",
+    "url": "/real-estate/static/media/house-6.1c3f45cd.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/real-estate/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/real-estate/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/real-estate/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/real-estate/static/media/slick.f97e3bbf.svg"
+  }
+]);
